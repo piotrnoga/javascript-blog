c0c6882ddaecaa32f3d483d3605429269bc5c137
Piotr Noga <piotrnoga.dev@gmail.com>
2024-09-15 15:15:11 +0200

    Create HTML of the link for the list

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@
 //     const links = document.querySelectorAll(".titles a");
 //     console.log("links:", links);
 // });
-{
 
+
+// FIND THE ARTICILE AFTER CLICK ON THE ELEMENT
+
+{
     const titleClickHandler = function(event){
         const clickedElement = this;
         event.preventDefault();
@@ -54,5 +57,49 @@
     for(let link of links){
         link.addEventListener('click', titleClickHandler);
     }
+}
 
+// GENERATE ARTICLE TITLE LIST
+
+{
+    const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+    function generateTitleLinks(){
+    
+        /* [DONE] remove contents of titleList */
+    
+        document.querySelector(optTitleListSelector).innerHTML = '';
+    
+        /* [DONE] find all the articles and save them to variable: articles */
+    
+        const articles = document.querySelectorAll(optArticleSelector);
+    
+        let html = '';
+    
+        for(let article of articles){
+            /* get the article id */
+
+            const articleId = article.getAttribute('id');
+            
+            /* find the title element */
+            /* get the title from the title element */
+            
+            const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+            
+            /* create HTML of the link */
+            
+            const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+            console.log('titleOfArticle:', linkHTML);
+            
+            /* insert link into html variable */
+            
+            html = html + linkHTML;
+        }
+
+        // titleListSelector.innerHTML = html;
+    }
+
+    generateTitleLinks();
 }
